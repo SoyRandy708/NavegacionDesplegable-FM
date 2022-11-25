0f3a76412780d9050d9e369f1f2c7198e71916ac
SoyRandy708 <randy708ochoa@gmail.com>
2022-11-24 23:00:18 -0600

    Pequeños arreglos hechos

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const menuHamburguesa = document.querySelector("#menu-desplegable")
 const menuDesplegable = document.querySelector("#nav-mobile")
+const AbrirMenuDesplegable = document.querySelector("#menu-desplegable")
 const CerrarMenuDesplegable = document.querySelector("#cerrar-nav-mobile")
 const CaracteristicasDes = document.querySelector("#caracteristicas-nav-desktop")
 const CaracteristicasNavDes = document.querySelector("#nav-desktop-caracteristicas")
@@ -11,19 +11,17 @@ const EmpresaMob = document.querySelector("#empresa-nav-mobile")
 const EmpresaNavMob = document.querySelector("#nav-mobile-empresa")
 
 
-menuHamburguesa.addEventListener("click", MenuMobileToggle)
-CerrarMenuDesplegable.addEventListener("click", CloseMenuMobile)
-CaracteristicasDes.addEventListener("mouseover", MostrarCaracteristicasDesktop)
-EmpresaDes.addEventListener("mouseover", MostrarEmpresaDesktop)
+AbrirMenuDesplegable.addEventListener("click", MenuMobileToggle)
+CerrarMenuDesplegable.addEventListener("click", MenuMobileToggle)
+
+CaracteristicasDes.addEventListener("click", MostrarCaracteristicasDesktop)
+EmpresaDes.addEventListener("click", MostrarEmpresaDesktop)
 CaracteristicasMob.addEventListener("click", MostrarCaracteristicasMobile)
 EmpresaMob.addEventListener("click", MostrarEmpresaMobile)
 
 
 function MenuMobileToggle() {
-    menuDesplegable.classList.toggle("inactive")
-}
-function CloseMenuMobile() {
-    menuDesplegable.classList.add("inactive")
+    menuDesplegable.classList.toggle("mostrar-nav-mobile")
 }
 function MostrarCaracteristicasDesktop() {
     CaracteristicasNavDes.classList.toggle("inactive")
